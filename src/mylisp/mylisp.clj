eb3ae00421f97f741542cb147d0cd980b0876da0
Anders Eliasson <anders.eliasson@digitalroute.com>
2015-01-05 19:17:19 +0100

    first commit

---
+++ src/mylisp/mylisp.clj
@@ -0,0 +1,122 @@
+(ns mylisp.mylisp
+  (:use [clojure.pprint])
+  (:require [instaparse.core :as insta]
+            [clojure.core.match :refer [match]]))
+
+(defmacro dbg[x] `(let [x# ~x] (println (java.lang.System/currentTimeMillis) ": " '~x "=" x#) x#))
+
+(defmulti ast->clj first) 
+
+(defmulti special-char-macro (fn [sc expr] sc))
+
+(defn interleave-with [args delim] (apply str (butlast (interleave args (map (fn [_] delim) args)))))
+
+(defn grammar []
+  (str
+     "
+     PROGRAM = EXPR*  
+     EXPR = OPTIONAL-SPACE (SINGLE-LINE-COMMENT / NUMBER / (STRING |  LIST | VECTOR | MAP | SYMBOL | KEYWORD | (READER-MACRO | (SPECIAL-CHARS EXPR))))
+     SPECIAL-CHARS = QUOTE | UNQUOTE | UNQUOTE-SPLICING | DEREF | SYNTAX-QUOTE
+     UNQUOTE = <'~'>
+     UNQUOTE-SPLICING = <'~@'>
+     QUOTE = <'\\''>
+     SYNTAX-QUOTE = <'`'>
+     DEREF = <'@'>
+     READER-MACRO = <'#'> (SET | LIST | ('_' EXPR))
+     LIST = (<'('> OPTIONAL-SPACE <')'>) |  (<'('> ARGS <')'>)
+     VECTOR = (<'['> OPTIONAL-SPACE <']'>) |  (<'['> ARGS <']'>)
+     MAP = (<'{'> OPTIONAL-SPACE <'}'>) |  (<'{'> ARGS <'}'>)
+     SET = (<'{'> OPTIONAL-SPACE <'}'>) |  (<'{'> ARGS <'}'>)
+     <ARGS> = (EXPR SPACE)* EXPR
+     KEYWORD = <':'> SYMBOL
+     SYMBOL = #'[^\\'`~@\\(\\)\\[\\]{}:;#,\\^ \t\n0123456789]' #'[^\\'`~@\\(\\)\\[\\]{}:;#,\\^ \t\n]*'
+     SINGLE-LINE-COMMENT = <';'> #'.'* NEW-LINE 
+     STRING = <'\\\"'> #'([^\"\\\\]|\\\\.)*' <'\\\"'>
+     NUMBER = (INTEGER | FLOAT | FRACTION) | (SIGN (INTEGER | FLOAT | FRACTION))
+     SIGN = (PLUS-SIGN | MINUS-SIGN)
+     <PLUS-SIGN> = '+'
+     <MINUS-SIGN> = '-' 
+     FLOAT = (DEC-INT <'.'> DEC-INT) | 
+             (DEC-INT <'.'>)
+     INTEGER = DEC-INT | HEX-INT 
+     FRACTION = INTEGER <'/'> INTEGER
+     DEC-INT = #'[0-9]+'
+     HEX-INT = <'0'> <'x' | 'X'> #'[0-9a-fA-F]+'
+     ANY-CHAR = #'.'
+     NEW-LINE = '\n' | Epsilon
+     <SPACE> = <#'[ \t\n,]+'>
+     <OPTIONAL-SPACE> = <#'[ \t\n,]*'>
+     "))
+
+(defn mylisp->ast
+  ([text start]
+    (insta/parse (insta/parser (grammar) :start start) text))
+  ([text] (mylisp->ast text :PROGRAM)))
+
+
+(defn expr-fn [& args]
+  (match 
+    (vec args)
+	     [[:SPECIAL-CHARS [sc]] expr]
+       (condp = sc
+         :QUOTE `'~expr
+         :UNQUOTE `(unquote ~expr)
+         :UNQUOTE-SPLICING `(unquote-splicing ~expr)
+         :DEREF `(deref ~expr)
+         :SYNTAX-QUOTE (read-string (str "`" (pr-str expr)))
+         )
+       [v c]
+       v
+	     [v]
+	     v))
+
+(defn number-fn [& args]
+  (match (vec args)
+    [[:SIGN "-"] v] (- v)
+    [[:SIGN "+"] v] v
+    [v] v)) 
+(defn float-fn 
+  ([v]
+    (float-fn v 0))
+  ([v1 v2]
+    (read-string (format "%s.%s" v1 v2))))
+
+(defn reader-macro-fn 
+  ([expr]
+    (cond 
+      (instance? java.util.Set expr) expr
+      (instance? java.util.List expr) (read-string (format "#%s" (pr-str expr)))))
+  ([token expr]
+    (condp = token
+      "_"
+      `(comment ~expr))))
+
+(def ast->clj-map 
+  {:SYMBOL (fn [& args] (symbol (apply str args)))
+   :DEC-INT read-string
+   :INTEGER identity
+   :NUMBER number-fn
+   :EXPR expr-fn
+   :LIST (fn [& args] `~args)
+   :VECTOR (fn [& args] `~(vec args))
+   :FLOAT float-fn
+   :MAP (fn [& args] (apply hash-map args))
+   :SET (fn [& args] (into #{} args))
+   :READER-MACRO reader-macro-fn
+   :FRACTION (fn [v1 v2] (/ v1 v2))
+   :STRING identity
+   :SINGLE-LINE-COMMENT (fn [& args] nil)
+   :NEW-LINE (fn [])
+   :KEYWORD keyword
+   :PROGRAM (fn [& args] `(do ~@args))
+   :HEX-INT (fn [expr] (read-string (format "0x%s" expr)))
+   }
+  )
+
+(defn ast->clj [ast]
+  (insta/transform
+    ast->clj-map 
+    ast))
+
+(defn my-eval [text] 
+  (->  text mylisp->ast ast->clj eval))
---
+++ test/mylisp/core_test.clj
@@ -0,0 +1,7 @@
+(ns mylisp1.core-test
+  (:require [clojure.test :refer :all]
+            [mylisp1.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
