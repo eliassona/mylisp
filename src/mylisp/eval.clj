8c28136ba428b0537221e2d4d4bf7b94d599a56c
Anders Eliasson <anders.eliasson@digitalroute.com>
2015-01-10 10:21:05 +0100

    a meta circular evaluator with protocols

--- src/mylisp/eval.clj
+++ src/mylisp/eval.clj
@@ -11,19 +11,19 @@
   (assert (= (count arg-names) (count args)))
   (my-eval impl (reduce (fn [acc [n v]] (assoc acc n v)) env (partition 2 (interleave arg-names args)))))
 
+
+(def pre-def-map 
+  {"+" + , "-" -})
+
 (defn fn-predefined [name args env]
-  (apply 
-    ({
-       "+" +
-       "-" -
-      } name) (map #(my-eval % env) args)))
+  (apply (pre-def-map name) (map #(my-eval % env) args)))
 
 (defn my-apply [[name & args] env]
   (let [n (str name)]
     (condp = n
       "def" (swap! vars assoc (first args) (-> args second (my-eval env)))
       "fn" args
-      (if-let [the-fn ((merge @vars env) name)] 
+      (if-let [the-fn (my-eval name env)] 
         (fn-app the-fn args env)
         (fn-predefined n args env))
         )))
