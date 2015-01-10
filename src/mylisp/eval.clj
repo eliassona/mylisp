1445dea18b0ecbce573ce1e1cc83ddcf7c4543c2
Anders Eliasson <anders.eliasson@digitalroute.com>
2015-01-10 12:03:19 +0100

    some more functions like first and rest etc

--- src/mylisp/eval.clj
+++ src/mylisp/eval.clj
@@ -11,14 +11,22 @@
   (assert (= (count arg-names) (count args)))
   (my-eval impl (reduce (fn [acc [n v]] (assoc acc n v)) env (partition 2 (interleave arg-names args)))))
 
+(defmacro apply-macro [name args env]
+ `(~(symbol name) ~@args)
+ )
 
 (def pre-def-map 
-  {"+" + , "-" -})
+  {"+" + , "-" -, "=" =, ">" >, "<" <, "not" not})
 
 (defn fn-predefined [name args env]
   (condp = name
     "quote" (first args) ;TODO is first correct here
     "unquote" (my-eval (my-eval (first args) env) env)
+    "if" (if (my-eval (first args) env) (my-eval (nth args 1) env) (my-eval (nth args 2) env))
+    "and" (reduce (fn [acc b] (and acc b)) (map #(my-eval % env) args))
+    "or" (reduce (fn [acc b] (or acc b)) (map #(my-eval % env) args))
+    "first" (first (map #(my-eval % env) (first args)))
+    "rest" (rest (map #(my-eval % env) (first args)))
     (if-let [f (pre-def-map name)]
       (apply f (map #(my-eval % env) args))
       (println (format "Symbol %s can't be found" name)))))
@@ -38,7 +46,18 @@
   String (my-eval [s _] s)
   clojure.lang.Symbol (my-eval [s env] ((merge @vars env) s))
   Number (my-eval [n _] n)
-  clojure.lang.Keyword (my-eval [k _] k))
+  clojure.lang.Keyword (my-eval [k _] k)
+  Boolean (my-eval [b _] b)
+  
+  )
 
 (defmacro evl [expr]
   `(my-eval '~expr {}))
+
+
+
+(evl (def >= (fn [v1 v2] (or (> v1 v2) (= v1 v2)))))
+(evl (def <= (fn [v1 v2] (or (< v1 v2) (= v1 v2)))))
+(evl (def abs (fn [v] (if (< v 0) (- v) v))))
+(evl (def max (fn [v1 v2] (if (> v1 v2) v1 v2))))
+(evl (def min (fn [v1 v2] (if (< v1 v2) v1 v2))))
